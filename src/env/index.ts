262e59a80efdea8ed7e9dff17ba6607c24845d93
gsrosa <gui.rosa.soares@gmail.com>
2026-04-28 22:50:02 -0300

    fix: env

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,16 +1,16 @@
-import { createEnv } from "@t3-oss/env-core";
-import { z } from "zod";
+import { createEnv } from '@t3-oss/env-core';
+import { z } from 'zod';
 
 type RuntimeEnv = Record<string, string | number | boolean | undefined>;
 
 export const env = createEnv({
-  clientPrefix: "NEXT_",
+  clientPrefix: 'NEXT_',
   client: {
     NEXT_PUBLIC_REMOTE_AI_ASSISTANT_URL: z.url(),
     NEXT_PUBLIC_REMOTE_USER_APP_URL: z.url(),
     NEXT_PUBLIC_REMOTE_PAYMENT_APP_URL: z.url(),
     NEXT_PUBLIC_API_URL: z.url(),
   },
-  runtimeEnv: import.meta.env as RuntimeEnv,
+  runtimeEnv: process.env as RuntimeEnv,
   emptyStringAsUndefined: true,
 });
